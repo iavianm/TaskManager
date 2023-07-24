c2a6d02687fceb98d870e982d292c467d87b7f48
iavianm <iavianm@mail.ru>
2023-07-25 00:56:26 +0300

    update factories

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :name, :description] do |n|
     "string#{n}"
+  end
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+  sequence :avatar do |n|
+    "avatar#{n}#{SecureRandom.urlsafe_base64(8)}.com"
+  end
+  sequence :expired_at do |n|
+    "2023-07-#{n}"
   end
 end
