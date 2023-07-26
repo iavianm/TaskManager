6accba4d052063d444036f86bff50663394d311a
Mikhail Iavian <85059359+iavianm@users.noreply.github.com>
2023-07-26 15:59:38 +0300

    Feature/models (#4)

---
+++ test/factories/sequence.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  sequence :string, aliases: [:first_name, :last_name, :password, :name, :description] do |n|
+    "string#{n}"
+  end
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+  sequence :avatar do |n|
+    "avatar#{n}#{SecureRandom.urlsafe_base64(8)}.com"
+  end
+  sequence :expired_at do |n|
+    DateTime.now + n
+  end
+end
