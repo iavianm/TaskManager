6dbee34095c8d6c9bf2281b3a269e57d6656cfe6
iavianm <iavianm@mail.ru>
2023-07-31 23:49:24 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
-require 'application_responder'
-
 class ApplicationController < ActionController::Base
-  self.responder = ApplicationResponder
-  respond_to :html
-
   include AuthHelper
   helper_method :current_user
 end
