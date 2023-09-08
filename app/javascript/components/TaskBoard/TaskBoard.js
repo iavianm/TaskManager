7aeb4d2a971ec2025b85795bf0bd4f9e87efc04a
iavianm <iavianm@mail.ru>
2023-09-08 23:11:01 +0300

    add column header

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -6,6 +6,7 @@ import { propOr } from 'ramda';
 
 import Task from '../Task';
 import TasksRepository from '../../repositories/TasksRepository';
+import ColumnHeader from '../ColumnHeader';
 
 const STATES = [
   { key: 'new_task', value: 'New' },
@@ -46,6 +47,23 @@ function TaskBoard() {
     });
   };
 
+  const loadColumnMore = (state, page = 1, perPage = 10) => {
+    loadColumn(state, page, perPage).then(({ data }) => {
+      setBoardCards((prevState) => {
+        const currentCards = prevState[state]?.cards || [];
+        const currentMeta = prevState[state]?.meta || {};
+
+        const newCards = [...currentCards, ...data.items];
+        const newMeta = { ...currentMeta, ...data.meta };
+
+        return {
+          ...prevState,
+          [state]: { cards: newCards, meta: newMeta },
+        };
+      });
+    });
+  };
+
   const generateBoard = () => {
     const newBoard = {
       columns: STATES.map(({ key, value }) => ({
@@ -66,7 +84,14 @@ function TaskBoard() {
   useEffect(() => loadBoard(), []);
   useEffect(() => generateBoard(), [boardCards]);
 
-  return <KanbanBoard renderCard={(card) => <Task task={card} />}>{board}</KanbanBoard>;
+  return (
+    <KanbanBoard
+      renderCard={(card) => <Task task={card} />}
+      renderColumnHeader={(column) => <ColumnHeader column={column} onLoadMore={loadColumnMore} />}
+    >
+      {board}
+    </KanbanBoard>
+  );
 }
 
 export default TaskBoard;
