72e00034ce73821a4b22465287580ab0cb1784b6
iavianm <iavianm@mail.ru>
2023-09-21 13:28:56 +0300

    fix

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -25,7 +25,7 @@ class Api::V1::TasksController < Api::V1::ApplicationController
   end
 
   def update
-    task = Task.find(task_params[:id])
+    task = Task.find(params[:id])
     task.update(task_params)
 
     respond_with(task, serializer: TaskSerializer)
