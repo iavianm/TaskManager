704df6f3d5e0a1661066be881e559263a3dd960b
iavianm <iavianm@mail.ru>
2023-10-10 09:14:07 +0300

    fix

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -4,8 +4,6 @@ class Api::V1::TasksController < Api::V1::ApplicationController
   def index
     tasks = Task.all.ransack(ransack_params).result.page(page).per(per_page)
 
-    pp tasks.count
-
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
 
