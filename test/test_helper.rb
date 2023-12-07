7529febddb61ad95a616c18c35642a916be0b7f1
iavianm <iavianm@mail.ru>
2023-12-07 13:45:18 +0200

    fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,23 +1,17 @@
-ENV['RAILS_ENV'] ||= 'test'
-
 require 'simplecov'
-# SimpleCov.coverage_dir('/task_manager/coverage')
-SimpleCov.start('rails')
-
 require 'coveralls'
-Coveralls.wear!('rails')
 
+SimpleCov.start
+Coveralls.wear!
+
+ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
   include AuthHelper
-  # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
