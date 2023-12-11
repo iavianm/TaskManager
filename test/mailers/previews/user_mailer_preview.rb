66a99d02c6fc0bad1f6ef0631da5bbef6fe66a95
iavianm <iavianm@mail.ru>
2023-12-11 10:52:40 +0200

    add preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,19 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def task_created
+    user = User.first
+    task = Task.first
+    UserMailer.with(user: user, task: task).task_notification(:created)
+  end
 
+  def task_updated
+    user = User.first
+    task = Task.first
+    UserMailer.with(user: user, task: task).task_notification(:updated)
+  end
+
+  def task_deleted
+    user = User.first
+    task = Task.first
+    UserMailer.with(user: user, task: task).task_notification(:deleted)
+  end
 end
