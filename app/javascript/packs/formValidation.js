27cfb745051364451e7ad18727a088ba10577442
iavianm <iavianm@mail.ru>
2024-01-17 23:08:05 +0200

    fix

--- app/javascript/packs/formValidation.js
+++ app/javascript/packs/formValidation.js
@@ -8,60 +8,47 @@ document.addEventListener('DOMContentLoaded', () => {
   const passwordField = document.getElementById('password_field');
   const passwordConfirmationField = document.getElementById('password_confirmation_field');
 
-  const EMAIL_REGEXP_PART1 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@/;
-  const EMAIL_REGEXP_PART2 = /[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?/;
-  const EMAIL_REGEXP_PART3 = /\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?/;
-  const EMAIL_REGEXP_PART4 = /(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  const EMAIL_REGEXP = new RegExp(
-    EMAIL_REGEXP_PART1.source + EMAIL_REGEXP_PART2.source + EMAIL_REGEXP_PART3.source + EMAIL_REGEXP_PART4.source,
-  );
+  const EMAIL_REGEXP = /^\S+@\S+\.\S+$/;
 
   const validateEmail = (email) => EMAIL_REGEXP.test(String(email));
 
+  if (submitPassButton) {
+    submitPassButton.classList.add('disabled', submitPassButton.disabled);
+  }
+
+  if (submitEmailButton) {
+    submitEmailButton.classList.add('disabled', submitEmailButton.disabled);
+  }
+
   const updateSubmitEmailButtonState = () => {
-    if (!validateEmail(emailField.value)) {
-      submitEmailButton.disabled = true;
-      submitEmailButton.classList.add('disabled');
-    } else {
-      submitEmailButton.classList.remove('disabled');
-      submitEmailButton.disabled = false;
+    const emailValue = emailField.value;
+    const isValidEmail = validateEmail(emailValue);
+    const isEmailEmpty = emailValue.length === 0;
+    submitEmailButton.disabled = !isValidEmail || isEmailEmpty;
+    submitEmailButton.classList.toggle('disabled', !isValidEmail || isEmailEmpty);
+    if (errorEmailMessage) {
+      errorEmailMessage.textContent =
+        (isEmailEmpty && 'Email is empty') || (!isValidEmail && 'Invalid email format') || '';
     }
   };
 
   if (emailField && submitEmailButton) {
-    emailField.addEventListener('input', () => {
-      if (errorEmailMessage) {
-        errorEmailMessage.textContent = '';
-      }
-      updateSubmitEmailButtonState();
-    });
-    updateSubmitEmailButtonState();
+    emailField.addEventListener('input', updateSubmitEmailButtonState);
   }
 
   const updateSubmitPassButtonState = () => {
-    const passwordsMatch = passwordField.value === passwordConfirmationField.value;
-    const passwordsNotEmpty = passwordField.value !== '' && passwordConfirmationField.value !== '';
-    if (!passwordsMatch || !passwordsNotEmpty) {
-      submitPassButton.disabled = true;
-      submitPassButton.classList.add('disabled');
-    } else {
-      submitPassButton.classList.remove('disabled');
-      submitPassButton.disabled = false;
-    }
-  };
+    const isPasswordShort = passwordField.value.length < 2;
+    const isPasswordMismatch = passwordField.value !== passwordConfirmationField.value;
 
-  const checkPasswords = () => {
-    if (passwordField.value !== passwordConfirmationField.value) {
-      errorPassMessage.textContent = 'Passwords do not match';
-    } else {
-      errorPassMessage.textContent = '';
-    }
-    updateSubmitPassButtonState();
+    submitPassButton.disabled = isPasswordShort || isPasswordMismatch;
+    submitPassButton.classList.toggle('disabled', submitPassButton.disabled);
+
+    errorPassMessage.textContent =
+      (isPasswordShort && 'Password is too short') || (isPasswordMismatch && 'Passwords do not match') || '';
   };
 
   if (passwordField && passwordConfirmationField && submitPassButton) {
-    passwordField.addEventListener('input', checkPasswords);
-    passwordConfirmationField.addEventListener('input', checkPasswords);
-    updateSubmitPassButtonState();
+    passwordField.addEventListener('input', updateSubmitPassButtonState);
+    passwordConfirmationField.addEventListener('input', updateSubmitPassButtonState);
   }
 });
